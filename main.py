bb14a6da4d5e15e71f6117de9eb9d1a294b88c12
kyontwises <kenny.yontwises@gmail.com>
2021-11-08 15:15:50 -0800

    Starting over with new way of storing goal/initial puzzle (rows/columns)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import heapq
 
 class Node:
-    def __init__(self,data,h,g,depth):
+    def __init__(self,data,h,g,depth,state):
         self.data = data
         self.children = []
         self.parent = None
         self.h = h #heuristic cost
-        self.g = g #cost to get to depth to depth
-        self.depth = depth
+        self.depth = depth #depth of node (g(n))
+        self.visited = False #in place of visited list just set to true when expanding
+        self.state = state
 
     def add_child(self,child):
         child.parent = self
@@ -18,9 +18,7 @@ def test_puzzle():
     #[4,5,6]
     #[7,8,0]
 
-
-
-    testPuzzle = [Node(1,0,1,0),Node(2,0,1,0),Node(3,0,1,0),Node(4,0,1,0),Node(5,0,1,0),Node(6,0,1,0),Node(7,0,1,0),Node(8,0,1,0),Node(0,0,1,0)]
+    testPuzzle = [1, 2, 3], [4,5,6], [7,8,0]
     return testPuzzle
 
 #return index where 0 is located     
@@ -34,45 +32,106 @@ def find_index(puzzle,num):
     # print(index)
     return index
 
-def UCS(test_tree):
-    rootIndex = find_index(test_tree,0) #create root
-    nodes = heapq.heapify(test_tree[rootIndex]) #create data structure to store initial state 
-    visited = []
-    depth = 0
-    path = []
-    expandCount = 0
-    while nodes:
-        if not nodes:
-            print("Empty queue. No solution")
-        
-        node = heapq.heappop(nodes)
-        visited.append(node)
-        if node.data == find_index(node.data) + 1:
-            path.append(node)
-        expandCount += expand_nodes(test_tree,find_index(test_tree,node.data),node)
-
-
-def expand_nodes(self,test_tree,index,node):
+def goal_state(problem):
+    goal_state = [1, 2, 3], [4,5,6], [7,8,0]
+    if(problem == goal_state):
+        return True
+    else:
+        return False
     
+
+#inspired by Professor's general search algorithm
+def UCS(problem):
+    rootIndex = find_index(problem,0) #get rood index
+    nodes = [] #create data structure to store initial state 
+    nodes.append(problem[rootIndex - 1])  #store root inside datastructure
+    #visited = [] #list of visited nodes 
+    path = [] #nodes we have seen already 
+    expandCount = 0 #number of nodes expanded
+
+    
+    while True:
+        if not nodes:
+            return 'failure'
+        
+        node = nodes.pop(0) #popoff front node
+        if node.visited is False:
+            expandCount += 1
+            node.visited = True
+        
+        if goal_state(node.state):
+            return 'Success + print more info...'
+        
+        print('')
+
+        
+         
+
+        
+
+
+
+def expand_nodes(node):
+    #012
+    #345
+    #678
+    expandCount = 0
     match index:
         case 0:
             expandCount = 2
-            node.add_child(test_tree[1])
-            node.add_child(test_tree[3])
+            node.add_child(problem[1])
+            node.add_child(problem[3])
             return expandCount
         case 1:
             expandCount = 3
-            node.add_child(test_tree[0])
-            node.add_child(test_tree[2])
-            node.add_child(test_tree[4])
+            node.add_child(problem[0])
+            node.add_child(problem[2])
+            node.add_child(problem[4])
             return expandCount
         case 2: 
             expandCount = 2
-            node.add_child(test_tree[1])
-            node.add_child(test_tree[5])
+            node.add_child(problem[1])
+            node.add_child(problem[5])
             return expandCount
+        case 3: 
+            expandCount = 3
+            node.add_child(problem[0])
+            node.add_child(problem[4])
+            node.add_child(problem[6])
+            return expandCount
+        case 4: 
+            expandCount = 4
+            node.add_child(problem[1])
+            node.add_child(problem[3])
+            node.add_child(problem[5])
+            node.add_child(problem[7])
+            return expandCount
+        case 5: 
+            expandCount = 3
+            node.add_child(problem[2])
+            node.add_child(problem[4])
+            node.add_child(problem[8])
+            return expandCount
+        case 6: 
+            expandCount = 2
+            node.add_child(problem[3])
+            node.add_child(problem[7])
+            return expandCount
+        case 7:
+            expandCount = 3
+            node.add_child(problem[4])
+            node.add_child(problem[6])
+            node.add_child(problem[8])
+            return expandCount
+        case 8:
+            expandCount = 2
+            node.add_child(problem[5])
+            node.add_child(problem[7])
+            return expandCount
+    
 
-
+            
+            
 
 
 
@@ -85,7 +144,11 @@ def swap(tree,pos1,pos2):
 
 def main():
     test = test_puzzle()
-    find_index(test)
+    # for node in test:
+    #     print(str(node.data) + ',')
+    
+    expandCount = UCS(test)
+    print('expand count: ' + str(expandCount))
     
 
 if __name__== "__main__":
