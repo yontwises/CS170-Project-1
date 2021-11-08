f7c243525b971778ea00f509607d3cf30fc3a047
Yontwises <kenny.yontwises@gmail.com>
2021-11-08 01:48:34 -0800

    expand nodes WIP

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import heapq
 
 class Node:
-    def __init__(self,data):
+    def __init__(self,data,h,g):
         self.data = data
         self.children = []
         self.parent = None
+        self.h = h #heuristic cost
+        self.g = g #cost to get to depth to depth
 
     def add_child(self,child):
         child.parent = self
@@ -17,7 +19,7 @@ def test_puzzle():
 
 
 
-    testPuzzle = [Node(1),Node(2),Node(3),Node(4),Node(5),Node(6),Node(7),Node(8),Node(0)]
+    testPuzzle = [Node(1,0,1),Node(2,0,1),Node(3,0,1),Node(4,0,1),Node(5,0,1),Node(6,0,1),Node(7,0,1),Node(8,0,1),Node(0,0,1)]
     return testPuzzle
 
 #return index where 0 is located     
@@ -35,17 +37,30 @@ def UCS(test_tree):
     rootIndex = find_index(test_tree,0) #create root
     nodes = heapq.heapify(test_tree[rootIndex]) #create data structure to store initial state 
     visited = []
+    depth = 0
+    path = []
     while nodes:
         if not nodes:
             print("Empty queue. No solution")
         
         node = heapq.heappop(nodes)
         visited.append(node)
+        depth = depth + 1
         if node.data == find_index(node.data) + 1:
-            nodes = 
+            path.append(node)
+
+
+def expand_nodes(self,test_tree,index):
+    match index:
+        case '0':
+            if 
 
 
 
+
+def swap(tree,pos1,pos2):
+    tree[pos1], tree[pos2] = tree[pos2], tree[pos1]
+    return tree
 
 
 
